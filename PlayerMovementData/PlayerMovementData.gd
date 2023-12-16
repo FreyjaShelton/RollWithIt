d8ee6412fd6ed9dc5ed207d5463dc72ea289d370
Freyja Shelton <freyjashelton@gmail.com>
2023-12-16 00:26:56 -0500

    add sample assets and wall jump, double jump and wall slide

--- PlayerMovementData/PlayerMovementData.gd
+++ PlayerMovementData/PlayerMovementData.gd
@@ -5,5 +5,7 @@ extends Resource
 @export var acceleration = 800.0
 @export var friction = 1000.0
 @export var air_resistance = 150.0
+@export var air_acceleration = 400.0
 @export var jump_velocity = -300.0
 @export var gravity_scale = 1.0
+@export var wall_slide_speed = .2
--- player.gd
+++ player.gd
@@ -1,33 +1,64 @@
 extends CharacterBody2D
 
 @export var movement_data : PlayerMovementData
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 @onready var coyote_jump_timer = $CoyoteJumpTimer
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var double_jump = false
+var wall_slide = false
 
 func _physics_process(delta):
 	apply_gravity(delta)
 	handle_jump()
+	handle_wall_jump()
 	
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
 	handle_acceleration(input_axis, delta)
+	handle_air_acceleration(input_axis, delta)
+	handle_wall_slide(input_axis, delta)
 	
 	var was_on_floor = is_on_floor()
 	move_and_slide()
 	handle_coyote_jump(was_on_floor)
 
 func apply_gravity(delta):
-	if not is_on_floor():
+	if not is_on_floor() and not wall_slide:
 		velocity.y += gravity * delta * movement_data.gravity_scale
 
+func handle_wall_jump():
+	if is_on_wall():
+		var wall_normal = get_wall_normal()
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.x = wall_normal.x * movement_data.speed
+			velocity.y = movement_data.jump_velocity
+
 func handle_jump():
+	if is_on_floor(): double_jump = true
+	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = movement_data.jump_velocity
 	if not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < movement_data.jump_velocity/2:
 			velocity.y = movement_data.jump_velocity/2
+		
+		if Input.is_action_just_pressed("ui_accept") and double_jump and not is_on_wall():
+			velocity.y = movement_data.jump_velocity * 0.8
+			double_jump = false
+
+func handle_wall_slide(input_axis, delta):
+	if not is_on_wall():
+		wall_slide = false
+		return
+
+	if is_on_wall() and input_axis != 0:
+		var wall_normal = get_wall_normal()
+		if input_axis != wall_normal.x:
+			wall_slide = true
+			velocity.y += gravity * delta * movement_data.wall_slide_speed
 
 func handle_coyote_jump(was_on_floor):
 	var just_left_ground = was_on_floor and not is_on_floor() and velocity.y >= 0
@@ -43,5 +74,11 @@ func apply_air_resistance(input_axis, delta):
 		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
 
 func handle_acceleration(input_axis, delta):
+	if not is_on_floor(): return
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.acceleration * delta)
+
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor(): return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.air_acceleration * delta)
